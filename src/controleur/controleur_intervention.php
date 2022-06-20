c8c8ce0d7d4edd3b999766c8f0bf61631f67dd5b
bayon <fabienbayon@yahoo.fr>
2022-06-20 11:58:07 +0000

    idk

--- src/controleur/controleur_intervention.php
+++ src/controleur/controleur_intervention.php
@@ -13,3 +13,18 @@ function actionlisteIntervention($twig, $db) {
      }
     echo $twig->render('listeIntervention.html.twig', array('listeIntervention' => $listeIntervention, 'idParcelle' => $_GET['idParcelle']));
 }
+
+
+function actionStats($twig, $db) {
+    $form = array();
+    $intervention = new Intervention($db);
+    $listeIntervention = $intervention->selectAllByIdParcelle($_GET['idParcelle']);
+    if(isset($_GET['idIntervention'])){
+        $currentDate = date("Y/m/d");
+        $intervention->update($_GET['idIntervention'], $currentDate);
+     }
+     if(isset($_GET['idDel'])){
+        $intervention->delete($_GET['idDel']);
+     }
+    echo $twig->render('statistiques.html.twig', array('listeIntervention' => $listeIntervention, 'idParcelle' => $_GET['idParcelle']));
+}
